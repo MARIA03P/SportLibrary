ddc85ee00b76e9e66df329b1c88c82fa168c88d6
PopovaM <popovam03@gmail.com>
2022-06-20 12:06:25 +0300

    Добавьте файлы проекта.

---
+++ Sportsman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportLibrary
+{
+    public enum SportType
+    {
+        Runner,
+        Jumper,
+        Footbol,
+        Biker
+            
+    }
+    public interface Sportsman
+    {
+        public object ToDo(params object[] massiv);
+
+        public double GetResult();
+    
+
+    }
+}

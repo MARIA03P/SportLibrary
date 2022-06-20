318bd155373a8b2d149cf3e757d1122a888b92f3
MARIA03P <90255821+MARIA03P@users.noreply.github.com>
2022-06-20 12:09:10 +0300

    Add files via upload

---
+++ SportLibrary/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportLibrary
+{
+    public static class Referee
+    {
+        /// <summary>
+        /// Сортирует согласно заданному параметру
+        /// </summary>
+        public static IEnumerable<object> SortResult(IEnumerable<Sportsman> items)
+        {
+            List<Sportsman> sportsman = new List<Sportsman>();
+            sportsman.AddRange(items);
+            return sportsman.OrderBy(item => item.GetResult()).AsEnumerable();
+
+            
+        }
+        public static IEnumerable<int> GetStartPosition( int min, int max )
+        {
+            Random rnd = new Random();
+            int count = max - min;
+            List<int> position = new List<int>();
+            while (position.Count != count)
+            {
+                int value = rnd.Next(min, max);
+                bool result = position.Contains(value);
+                if (!result)
+                    position.Add(value);
+            }
+            return position;
+        }
+    }
+}
